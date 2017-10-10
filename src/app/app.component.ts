a37b13fea2809d89c755d7b6ea9b97f4a242bfe1
xarilo <xavier.rins@gmail.com>
2017-10-10 02:44:58 +0200

    UPLOAD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  private apiUrl = 'http://localhost:8080/api/usuarios'; // para que solo se pueda llamar desde este appcomponent
+
+  data: {
+    nombre
+    // puedo poner lo que sea porque lo recoje de date y lo mete aqui
+  };
+  usuario: {
+    nombre,
+    apellidos,
+    email,
+    password
+  };
+
+  onSubmit() {
+    // Mostramos el objeto usuario
+    console.log(this.usuario);
+    this.postUsers(); // ejecuto el post cuando hago click
+    window.location.reload();
+  }
+  onSubmit2() {
+    // Mostramos el objeto usuario
+    console.log(this.usuario);
+    this.deleteUsers();
+    //window.location.reload();
+  }
+
+  constructor (private http: Http) {
+    console.log('Hello user');
+    this.getUsers();
+    this.getDataRequest();
+    this.usuario = {
+      'nombre': '',
+      'apellidos': '',
+      'email': '',
+      'password': ''
+    };
+  }
+
+  getDataRequest() { // creo la request a la api
+    return this.http.get(this.apiUrl).map((res: Response) => res.json());
+  }
+
+  postDataRequest() {
+    return this.http.post(this.apiUrl, this.usuario);
+  }
+
+  deleteDataRequest() {
+    return this.http.delete(this.apiUrl, this.usuario);
+  }
+
+  getUsers() {
+    this.getDataRequest().subscribe(data => { // ejecuto la request del get
+      console.log(data);
+      this.data = data;
+    });
+
+  }
+
+  postUsers() {
+    this.postDataRequest().subscribe();
+  }
+
+  deleteUsers() {
+    this.deleteDataRequest().subscribe();
+  }
+
 }
